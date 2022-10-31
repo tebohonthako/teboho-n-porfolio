445ba2ef293a1f7187fc96d1bfb91b20afae8c69
Teboho Nthako <tjaynthako@gmail.com>
2022-10-31 17:21:06 +0200

    fixed project structure

---
+++ README.md
@@ -0,0 +1,62 @@
+### Description
+
+A simple portfolio to demonstrate my work as a developer built with React. 
+
+### [live preview](https://teboho-nthako-portfolio.github.io/react-portfolio/)
+
+[![react portfolio](https://teboho-nthako-portfolio.netlify.app)
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone https://teboho-nthako-portfolio.netlify.app/.git</pre>
+ 
+Install required dependencies
+
+<pre>npm install</pre>
+
+
+Start the server
+
+<pre>npm start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
+
+### Thanks
+
+### How to bootstrap in react
+npm install bootstrap --save // Inside of your command line
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // Inside of your index.js
+
+### How to Add Bootstrap to React
+//1.run following commnad in cmd:
+
+
+npm install bootstrap --save
+
+//you can now use bootstrap component. example:
+import { Button } from 'react-bootstrap';
+
+//2.or  add this to index.js:
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+  crossorigin="anonymous"
+/>
+
+
+### Deployment
+[![Netlify Status](https://api.netlify.com/api/v1/badges/4f6b20e4-97d2-44e2-9707-9a1adf9c4e17/deploy-status)](https://app.netlify.com/sites/teboho-nthako-portfolio/deploys)
---
+++ src/content_option.js
@@ -0,0 +1,106 @@
+const logotext = "TEBOHO";
+const meta = {
+    title: "Teboho Nthako",
+    description: "Aspirant Full-stack developer_ and I love to craft solid & scalable software with great user-experiences.",
+};
+
+const introdata = {
+    title: "I’m Teboho Nthako",
+    animated: {
+        first: "I'm a self-motivated software developer",
+        second: "A JavaScript enthusiast",
+        third: "Love building cool web applications",
+    },
+    description: "I love combining the worlds of logic and creative design to make beautiful, accessible and user-friendly websites and applications. I value myself on being a great contribution to any team. And that is why I want to be a developer at SovTech. ",
+    your_img_url: "https://www.instagram.com/p/CZpM3SaM8dI/",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "Hello World! My name is Teboho Nthako, An aspirant full-stack developer based in Johannesburg. I'm a Biological science Honors graduate who fell in love with code and the problem-solving of it all. I love to craft solid & scalable software with great user-experiences. In my spare time you'll catch me on Youtube watching my favorite vloggers, cooking, creating content or working on my personal projects that expose me to new programming languages and technologies. #Tech is the future!  ",
+};
+const worktimeline = [{
+        jobtitle: ".NET Programming Apprentice/Intern",
+        where: "Deviare",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "ReactJS",
+        value: 80,
+    },
+    {
+        name: "GitHub",
+        value: 85,
+    },
+    {
+        name: "Netlify",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Software developemnet / Web development",
+        description: "my projects are front-end and back-end, and love seeing my work come to life.",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "my MovieLand react-app.",
+        link: "https://teboos-movieland.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Clone of the Tesla Landing page [ZAIO Bootcamp].",
+        link: "https://teboho-tesla-landing-page.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "My Portfolio",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tjaynthako@gmail.com",
+    YOUR_FONE: "(+27) 7644-34084",
+    description: "Get in touch with me.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_crt4r4b",
+    YOUR_TEMPLATE_ID: "template_jvcu046",
+    YOUR_USER_ID: "DKPUublal98hza8Ko",
+};
+
+const socialprofils = {
+    github:   "https://github.com/tebohonthako",
+    linkedin: "https://www.linkedin.com/in/tebohonthako/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
